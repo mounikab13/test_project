078e5ca5f1980c72d4845595275ebe31f72a26e1
mounikab13 <mounika.boddhireddy@gmail.com>
2019-12-31 10:51:32 +0100

    added few scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,12 @@
 
 basedir="/home/mounika/bioinformatics/test_project"
 
-wget -P $basedir  
+wget -P $basedir "file url"
+
+./guppy_basecaller -i $basedir -o "output folder location" -c "config file"
+
+pycoQC -f "folder containing sequencing_summary.txt from guppy basecalling" -o "output folder to report.html"
+
+minimap2 -a "fastq file from guppy" "fasta file" > output sam file
+
+ 
