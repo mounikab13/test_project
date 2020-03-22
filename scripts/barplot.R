b8f0326def290c9056c74cd4aa8b694c0cf90109
mounika_boddireddy <49946547+mounikab13@users.noreply.github.com>
2020-03-22 16:59:15 +0100

    Update barplot.R

--- scripts/barplot.R
+++ scripts/barplot.R
@@ -13,11 +13,13 @@ ggplot(data,aes(y=c(nanocount1,illumina_reads),x=c(desc,desc))) + geom_bar(posit
 ggplot(data,aes(y=illumina_reads,x=desc)) + geom_bar(position = "dodge",stat = "identity") + scale_y_log10()
 
 condition<-c(rep(c("nanocount","illumina"),5))
-value<-c(13361612,138137622,16710724,229692250,33146409,227818,60021,168079,18295,60676)
-type<-c(desc,desc)
+value<-c(13361612,2210201946,5336687,198964151,9390854,583418846,60021,168079,18295,60676)
+type<-c("reads in fastq","reads in fastq","mapped to genome","mapped to genome","mapped to transcriptome","mapped to transcriptome","different transcripts","different transcripts","different genes","different genes")
+
 
 
 data1<-data.frame(type,condition,value)
 ggplot(data1, aes(fill=condition,y=value, x=type)) + 
   geom_bar(position="dodge", stat="identity") + scale_y_log10()
+
 
