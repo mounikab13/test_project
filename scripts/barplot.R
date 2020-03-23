8a9aa2266caefc57b3faa0809166cf1407649965
mounika_boddireddy <49946547+mounikab13@users.noreply.github.com>
2020-03-23 10:46:48 +0100

    Update barplot.R

--- scripts/barplot.R
+++ scripts/barplot.R
@@ -2,16 +2,6 @@
 
 library(ggplot2)
 
-nanocount1<-c(13361612,16710724,33146409,60021,18295)
-illumina_reads<-c(138137622,229692250,227818,168079,60676)
-desc<-c("reads in fastq","mapped to genome","mapped to transcriptome","different transcripts","different genes")
-data<-data.frame(desc,nanocount1,illumina_reads)
-names(nanocount1)<-desc
-names(illumina_reads)<-desc
-
-ggplot(data,aes(y=c(nanocount1,illumina_reads),x=c(desc,desc))) + geom_bar(position = "dodge",stat = "identity") + scale_y_log10()
-ggplot(data,aes(y=illumina_reads,x=desc)) + geom_bar(position = "dodge",stat = "identity") + scale_y_log10()
-
 condition<-c(rep(c("nanocount","illumina"),5))
 value<-c(13361612,2210201946,5336687,198964151,9390854,583418846,60021,168079,18295,60676)
 type<-c("reads in fastq","reads in fastq","mapped to genome","mapped to genome","mapped to transcriptome","mapped to transcriptome","different transcripts","different transcripts","different genes","different genes")
