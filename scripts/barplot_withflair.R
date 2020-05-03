db955e916627bd129c2d7db386bee2c422782103
mounika_boddireddy <49946547+mounikab13@users.noreply.github.com>
2020-05-03 21:31:23 +0200

    Add files via upload

---
+++ scripts/barplot_withflair.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+
+condition<-c(rep(c("nanocount","illumina","flair"),5))
+value<-c(13361612,2210201946,13361612,
+         5336687,198964151,9474547,
+         9390854,583418846,8929373,
+         60021,168079,545802,
+         18295,60676,61004)
+type<-c("reads in fastq","reads in fastq","reads in fastq",
+        "mapped to genome","mapped to genome","mapped to genome",
+        "mapped to transcriptome","mapped to transcriptome","mapped to transcriptome",
+        "different transcripts","different transcripts","different transcripts",
+        "different genes","different genes","different genes")
+
+
+
+data1<-data.frame(type,condition,value)
+ggplot(data1, aes(fill=condition,y=value, x=type)) + 
+  geom_bar(position="dodge", stat="identity") + scale_y_log10()
